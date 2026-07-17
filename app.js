d813530ed3b198c51779fedabd52c5ef1de0d4f7
Ahiz2 <caigoxiaoxu@users.noreply.github.com>
2026-07-17 21:25:30 +0800

    update blog

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ class ThemeManager {
 // 导航加载和文件管理
 class NavManager {
     constructor() {
-        this.assetVersion = '20260717-13';
+        this.assetVersion = '20260717-14';
         this.navData = { pages: [], notes: {} };
         this.entryMapBySrc = new Map();
         this.pageMap = new Map();
